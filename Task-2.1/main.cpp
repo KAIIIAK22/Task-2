cc70cd65f7a18f8732aa244599ac6a2f744c0c0f
Pavel Poltaves <dzhura17@yandex.ru>
2019-08-14 22:37:00 +0300

    Array instead of set

--- Task-2.1/main.cpp
+++ Task-2.1/main.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
-#include <set>
 
 using namespace std;
 
 int main()
 {
-   set<int> SchoolList;
+   int SchoolList[10];
    int a;
    for(int i=0;i<10;i++)
    {
        cout<<"Vvedi nomer kakoi-to shkolu: ";
-       cin>>a;
-       SchoolList.insert(a);
+       cin>>SchoolList[i];
    }
    cout<<"A teper vvedi svoyu shkolu: ";
    cin>>a;
-   if(SchoolList.find(a)!=SchoolList.end())
-       cout<<"Ya znayu etu shkolu!"<<endl;
-   else
-       cout<<"Ya ne znayu etu shkolu!"<<endl;
+   for(int i=0;i<10;i++)
+   {
+      if(a==SchoolList[i])
+      {
+          cout<<"Ya znayu etu shkolu!"<<endl;
+          break;
+      }
+      if (i==9) cout<<"Ya ne znayu etu shkolu!"<<endl;
+   }
 }
